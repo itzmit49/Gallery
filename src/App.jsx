50646d1cdff4a818cd57890e4e164bc153d4bf85
Amit Kumar <itzmit49@gmail.com>
2026-01-12 22:40:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,18 @@ import axios from "axios";
 const app = () => {
   const [userData, setuserData] = useState([]);
 
+  const [index, setindex] = useState(1);
+
   const getData = async () => {
     const response = await axios.get(
-      "https://picsum.photos/v2/list?page=2&limit=30"
-    );
+      `https://picsum.photos/v2/list?page=${index}&limit=21`
+    )
     setuserData(response.data);
   };
-  useEffect(function(){
-    getData()
-
-  },[])
-  let printUserData = <h3 className="text-gray-400">No User Available</h3>;
+  useEffect(function () {
+    getData();
+  }, [index]);
+  let printUserData = <h3 className="text-gray-400 text-xs absolute top-1/2 left-1/2 -translate-x-1/2">Loading...</h3>;
 
   if (userData.length > 0) {
     printUserData = userData.map(function (elem, idx) {
@@ -37,13 +38,34 @@ const app = () => {
 
   return (
     <div className="bg-black overflow-auto h-screen p-4 text-white ">
+
       <div className=" ml-20 mt-5 flex flex-wrap  gap-4">{printUserData}</div>
-      <div className="flex justify-center items-center p-5">
-        <button className="bg-amber-400 text-black px-4 py-4 "  >Prev</button>
-        <button>Next</button>
+      <div className="flex justify-center gap-6 items-center p-5">
+        <button 
+        className=" w-30 bg-amber-400 text-black px-4 py-4 text-sm cursor-pointer active:scale-95 rounded-xl "
+        onClick={()=>{
+
+
+          if(index>1)setindex(index-1)
+            setuserData([])
+
+        }}
+        >
+          Prev
+        </button>
+        <button 
+        className="w-30 bg-amber-400 text-black px-4 py-4 text-sm cursor-pointer active:scale-95 rounded-xl "
+         onClick={()=>{
+ setuserData([])
+          
+          setindex(index+1)
+
+          
+        }}>
+          Next
+        </button>
       </div>
     </div>
-  
   );
 };
 
